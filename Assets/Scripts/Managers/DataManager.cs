84cbf02b6428e58b9aa33f32632d9973c816bf0d
parkrye <parkrye@gmail.com>
2024-05-22 15:48:06 +0900

    디렉터링

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -18,36 +18,28 @@ public class DataManager : BaseManager
         foreach (var actor in actors)
         {
             if (Actors.ContainsKey(actor.Name) == false)
-            {
                 Actors[actor.Name] = actor;
-            }
         }
 
         var enemies = GameManager.Resource.LoadAll<EnemyData>("Datas/Actors/Enemies");
         foreach (var enemy in enemies)
         {
             if (Enemies.ContainsKey(enemy.Name) == false)
-            {
                 Enemies[enemy.Name] = enemy;
-            }
         }
 
         var products = GameManager.Resource.LoadAll<ProductData>("Datas/Products");
         foreach (var product in products)
         {
             if (Products.ContainsKey(product.Name) == false)
-            {
                 Products[product.Name] = product;
-            }
         }
 
         var equipments = GameManager.Resource.LoadAll<EquipmentData>("Datas/Products/Equipments");
         foreach (var equipment in equipments)
         {
             if (Equipments.ContainsKey(equipment.Name) == false)
-            {
                 Equipments[equipment.Name] = equipment;
-            }
         }
     }
 }
