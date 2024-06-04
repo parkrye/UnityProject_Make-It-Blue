1fdb8968b41ea5d1fb3fa4195859bf5ca887dd3c
parkrye <parkrye@gmail.com>
2024-06-04 11:01:17 +0900

    플레이어 카메라 벽 감지

---
+++ Assets/Scripts/Contents/Enviroments/CameraWallChecker.cs
@@ -0,0 +1,54 @@
+using Cinemachine;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class CameraWallChecker : MonoBehaviour
+{
+    private CinemachineTransposer _transposer;
+    private bool _isWall;
+    private float _initialZValue;
+
+    private void Awake()
+    {
+        _transposer = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTransposer>();
+        _initialZValue = _transposer.m_FollowOffset.z;
+    }
+
+    private void Start()
+    {
+        WallCheckRoutine().Forget();
+    }
+
+    private async UniTask WallCheckRoutine()
+    {
+        while (true)
+        {
+            await UniTask.Delay(100);
+            if (GameManager.System.PlayerActor == null)
+                continue;
+
+            if (Physics.Raycast(transform.position, GameManager.System.PlayerActor.CenterPosition - transform.position, out var frontHit))
+                _isWall = frontHit.collider.CompareTag("Wall");
+
+            if (_isWall)
+                continue;
+
+            if (Physics.SphereCast(transform.position, 0.2f, Vector3.up, out var aroundHit))
+                _isWall = aroundHit.collider.CompareTag("Wall");
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (_isWall)
+        {
+            if (_transposer.m_FollowOffset.z < 0f)
+                _transposer.m_FollowOffset.z = Mathf.Lerp(_transposer.m_FollowOffset.z, 0f, Time.deltaTime);
+        }
+        else
+        {
+            if (_transposer.m_FollowOffset.z > _initialZValue)
+                _transposer.m_FollowOffset.z = Mathf.Lerp(_transposer.m_FollowOffset.z, _initialZValue, Time.deltaTime);
+        }
+    }
+}
